fbcf6c4eba17325bdc5df73aa5e53b00c695cb46
mashuk-tamim <tamimmashuk328@gmail.com>
2024-01-25 15:57:23 +0600

    gallery scroll parallax project done

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,18 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Inter, Luckiest_Guy } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+// Font files can be located inside of `pages`
+const myFont = localFont({ src: './fonts/AVGARDD_2.woff', weight: '400', style: "normal" })
+// If loading a variable font, you don't need to specify the font weight
+const luckiest_guy = Luckiest_Guy({
+	subsets: ["latin"],
+	weight: [ "400"],
+    style: ["normal"],
+    display: 'swap'
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +25,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+		<html lang="en" className="m-0 p-0">
+			<body className={luckiest_guy.className}>{children}</body>
+		</html>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,22 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
-  },
-  plugins: [],
+	content: [
+		"./pages/**/*.{js,ts,jsx,tsx,mdx}",
+		"./components/**/*.{js,ts,jsx,tsx,mdx}",
+		"./app/**/*.{js,ts,jsx,tsx,mdx}",
+	],
+	theme: {
+		extend: {
+			backgroundImage: {
+				"gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+				"gradient-conic":
+					"conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+				buildings_bg:
+					"url(https://images.unsplash.com/photo-1497250681960-ef046c08a56e?q=80&w=987&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D)",
+			},
+		},
+	},
+	plugins: [],
 };
 export default config;
